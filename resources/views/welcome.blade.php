6687fb223a874358dce20090a498ce02877aa79d
lopheliaPertusa <m7208231@yandex.ru>
2026-05-10 22:41:25 +0300

    My local changes to welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
 
 @section('content')
 <div class="rentcar-page">
+<<<<<<< Updated upstream
     <!-- MAIN -->
     <div class="load-wrap">
         <div class="wheel-load">
@@ -24,6 +25,23 @@
                 <div class="col-sm-3">
                     <div class="wheel-logo">
                         <a href="index.html"><img src="{{ asset('assets/rentcar/images/logo.png') }}" alt=""></a>
+=======
+        <!-- MAIN -->
+	{{--        
+		<div class="load-wrap">
+            <div class="wheel-load">
+                <img src="{{ asset('assets/rentcar/images/loader.gif') }}" alt="" class="image">
+            </div>
+        </div>
+			
+        <div class="wheel-menu-wrap ">
+            <div class="container-fluid wheel-bg1">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="wheel-logo">
+                            <a href="index.html"><img src="{{ asset('assets/rentcar/images/logo.png') }}" alt=""></a>
+                        </div>
+>>>>>>> Stashed changes
                     </div>
                 </div>
                 <div class="col-sm-9 col-xs-12 padd-lr0">
@@ -44,6 +62,82 @@
                                         <li><a href="register.html">Register</a></li>
                                     </ul>
                                 </div>
+<<<<<<< Updated upstream
+=======
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-9 ">
+                        <div class="wheel-navigation">
+                            <nav id="dl-menu">
+                                <!-- class="dl-menu" -->
+                                <ul class="main-menu dl-menu">
+                                    <li class="menu-item   current-menu-parent menu-item-has-children   active-color ">
+                                        <a href="#">Легковые</a>
+                                        <ul class="sub-menu dl-submenu">
+                                            <li class="menu-item current-menu-item">
+                                                <a href="index.html">Легковой госномер - Е 777 КХ 77 </a>
+                                            </li>
+                                            <li class="menu-item current-menu-item">
+                                                <a href="index2.html">Легковой госномер - А 333 МО 50</a>
+                                            </li>
+                                            <li class="menu-item current-menu-item">
+                                                <a href="index2.html">Легковой госномер - М 111 ОТ 77</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item current-menu-parent menu-item-has-children  ">
+                                        <a href="#"> Мироавтобусы </a>
+                                        <!--class=" dl-submenu "-->
+                                        <ul class="sub-menu dl-submenu">
+                                            <li class="menu-item current-menu-item">
+                                                <a href="car-list-grid.html">Микроавтобус 1</a>
+                                            </li>
+                                            <li class="menu-item current-menu-item">
+                                                <a href="car-list-3col2.html">Микроавтобус 2</a>
+                                            </li>
+                                            <li class="menu-item current-menu-item">
+                                                <a href="car-listing-details.html">Микроавтобус 3</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item   ">
+                                        <a href="reservation1.html">Ваши заказы</a>
+                                    </li>
+                                    <li class="menu-item menu-item-has-children  ">
+                                        <a href="#">Все заказы</a>
+                                        <ul class="sub-menu">
+                                            <li class="menu-item "><a href="contact.html">Буряк В.А.</a></li>
+                                            <li class="menu-item "><a href="contact.html">Сахошкин П.В.</a></li>
+                                            <li class="menu-item "><a href="register.html">Фетищев А.В.</a></li>
+                                            <li class="menu-item "><a href="checkout.html">Корчажинский Б.В.</a></li>
+                                            <li class="menu-item "><a href="about.html">Ислямова Г.С.</a></li>
+                                            <li class="menu-item "><a href="shopping.html">Галимов Д.И.</a></li>
+
+                                        </ul>
+                                    </li>
+
+                                </ul>
+                                <div class="nav-menu-icon"><i></i></div>
+                            </nav>
+                            <!--              <a href="" class="wheel-cheader-but">Buy Now</a>-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+--}}
+        <div class="wheel-start">
+            <img src="{{ asset('assets/rentcar/images/bg1.jpg') }}" alt="" class="wheel-img">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="wheel-header text-center marg-lg-t140 marg-lg-b340 marg-md-t140 marg-md-b140 marg-sm-t70 ">
+            <!--                    <h5>Поиск автомобиля</h5>  -->
+                                <h3><span>Заполните форму заказа</span></h3>
+>>>>>>> Stashed changes
                             </div>
                         </div>
                     </div>
